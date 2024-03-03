6e8587c73b5d8892e2a20657ecc29357662c085e
Тимур Миндубаев <mindubaev.timurka@gmail.com>
2024-03-03 15:45:14 +0300

    Конец

--- Crocodile.cpp
+++ Crocodile.cpp
@@ -1,9 +1,58 @@
-﻿#include <iostream>
-#include <math.h>
-#include "geometry.h"
-
-using namespace std;
-
-int main()
-{
-}
+﻿//#include <iostream>
+//#include <math.h>
+//#include "geometry.h"
+//#include <windows.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	SetConsoleCP(1251);
+//	SetConsoleOutputCP(1251);
+//	int cond = 1;
+//	int cond2;
+//	while (cond != 0) {
+//		cout << R"(Программа для подсчёта площади геометрических фигур
+//Выберите действие:
+//0 - Выйти
+//1 - Вчислить площадь треугольника
+//2 - Вчислить площадь квадрата
+//3 - Вчислить площадь ромба
+//4 - Вчислить площадь прямоугольника
+//5 - Вывести количество посчетов площадей
+//)"; cin >> cond;
+//		switch (cond) {
+//		case 0:
+//			break;
+//		case 1:
+//			cout << R"(Выберите способ вычисления:
+//1 - По стороне и высоте
+//2 - По трем сторонам 
+//3 - По радиусу вписанной окружности и периметру
+//4 - По двум сторонам и углу между ними
+//)"; cin >> cond2;
+//			while (cond2 > 4 or cond2 < 1)
+//			{
+//				cout << "Несуществующий вариант. Попробуйте снова" << endl; cin >> cond2;
+//			}
+//			geometry::triangle(cond2);
+//			break;
+//		case 2:
+//			geometry::square();
+//			break;
+//		case 3:
+//			geometry::rhombus();
+//			break;
+//		case 4:
+//			geometry::rectangle();
+//			break;
+//		case 5:
+//			geometry::numb();
+//			break;
+//		default:
+//			cout << "Неверное действие. Попробуйте снова\n";
+//		}
+//		system("pause");
+//		system("cls");
+//	}
+//}
--- geometry.h
+++ geometry.h
@@ -1,81 +1,108 @@
 #pragma once
 
+using namespace std;
+
 class geometry
 {
 private:
     static int count;
     static double S;
 public:
-
-    static double triangle1() /*площадь треугольника по стороне и высоте*/
+    static double err(double numb)
     {
-        double a;
-        double h;
-        S = (h * a)/2;
-        count++;
-        return S;
+        while (numb < 0) {
+            cout << "Неверный размер. Попробуйте снова\n"; cin >> numb;
+        }
+        return numb;
     }
 
-    static double triangle2() // площадь треугольника по трем сторонам 
+    static void triangle(int num)
     {
-        double a;
-        double b;
-        double c;
-        double p = (a + b + c) / 2;
-        S = sqrt(p * (p - a) * (p - b) * (p - c));
+        double a = 0;
+        double b = 0;
+        double c = 0;
+        double p = 0;
+        switch (num) {
+        case 1://площадь треугольника по стороне и высоте
+            cout << "Введите сторону\n"; cin >> a;
+            a = err(a);
+            cout << "Введите высоту\n"; cin >> c;
+            c = err(c);
+            S = (c * a) / 2;
+            break;
+        case 2: // площадь треугольника по трем сторонам 
+            cout << "Введите первую сторону\n"; cin >> a;
+            a = err(a);
+            cout << "Введите вторую сторону\n"; cin >> b;
+            b = err(b);
+            cout << "Введите третью сторону\n"; cin >> c;
+            while (c < 0 or (a + b) <= c) {
+                cout << "Неверный размер. Попробуйте снова\n"; cin >> c;
+            }
+            p = (a + b + c) / 2;
+            S = sqrt(p * (p - a) * (p - b) * (p - c));
+            break;
+        case 3: // площадь треугольника по радиусу вписанной окружности и периметру
+            cout << "Введите радиус вписанной окружности\n"; cin >> a;
+            a = err(a);
+            cout << "Введите перимитр\n"; cin >> b;
+            b = err(b);
+            S = (a * b) / 2;
+            break;
+        case 4: // площадь треугольника по двум сторонам и углу между ними
+            cout << "Введите первую сторону\n"; cin >> a;
+            a = err(a);
+            cout << "Введите вторую сторону\n"; cin >> b;
+            b = err(b);
+            cout << "Введите размер угла\n"; cin >> c;
+            c = err(c);
+            S = a * b * sin(c);
+            break;
+        }
         count++;
-        return S;
+        cout << "Площадь равна: " << S << endl;
     }
 
-    static double triangle3() // площадь треугольника по радиусу вписанной окружности и периметру
-    {
-        double p;
-        double R;
-        S = (p * R) / 2;
-        count++;
-        return S;
-    }
-
-    static double triangle4() // площадь треугольника по двум сторонам и углу между ними
+    static void square() // площадь квадрата по длине стороны
     {
         double a;
-        double b;
-        double y;
-        S = a * b * sin(y);
-        count++;
-        return S;
-    }
-
-    static double square() // площадь квадрата по длине стороны
-    {
-        double a;
+        cout << "Введите сторону\n"; cin >> a;
+        a = err(a);
         S = a * a;
         count++;
-        return S;
+        cout << "Площадь равна: " << S << endl;
     }
 
-    static double rhombus() //площадь ромба по длине стороны и высоте
+    static void rhombus() //площадь ромба по длине стороны и высоте
     {
         double a = 0;
         double h = 0;
+        cout << "Введите сторону\n"; cin >> a;
+        a = err(a);
+        cout << "Введите высоту\n"; cin >> h;
+        h = err(h);
         S = a * h;
         count++;
-        return S;
+        cout << "Площадь равна: " << S << endl;
     }
 
-    static double rectangle() //площадь прямоугольника
+    static void rectangle() //площадь прямоугольника
     {
-        double S;
         double a = 0;
         double b = 0;
+        cout << "Введите первую сторону\n"; cin >> a;
+        a = err(a);
+        cout << "Введите вторую сторону\n"; cin >> b;
+        b = err(b);
         S = a * b;
         count++;
-        return S;
+        cout << "Площадь равна: " << S << endl;
     }
 
-    static int numb()
+    static void numb()
     {
-        return count;
+        cout << "Посчётов площадей: " << count << endl;
     }
 };
-
+int geometry::count = 0;
+double geometry::S = 0;
